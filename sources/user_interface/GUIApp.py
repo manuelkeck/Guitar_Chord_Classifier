fc726b182411f802840fec0a9e1f617d4c4efbe2
Manuel Keck <manuel@MacBook-Pro-von-Manuel.local>
2023-12-22 21:36:38 +0100

    Changes in GUI layout

--- sources/user_interface/GUIApp.py
+++ sources/user_interface/GUIApp.py
@@ -1,15 +1,12 @@
 import tkinter as tk
-
 from tkinter import ttk
 from screeninfo import get_monitors
 from sources.user_interface.GUIAppController import GUIAppController
-
 
 class GUIApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Recording Tool")
-        # root.resizable(False, False)
 
         # Windows size and positioning (based on main screen)
         window_width = 960
@@ -21,46 +18,55 @@ class GUIApp:
 
         style = ttk.Style()
 
-        style.configure("TFrame", background="black")
+        style.configure("TFrame", background="yellow")
         style.configure("TButton", padding=6, relief="flat", background="#d9d9d9")
 
+        self.content = tk.Frame(self.root)
+
         # Left frame #######################################################
-        self.left_frame = tk.Frame(root, width=480, height=500, bg="red")
-        self.left_frame.grid(row=0, column=0, sticky="nsew")
+        self.left_frame = tk.Frame(self.content, width=480, height=500, bg="blue")
+        self.left_frame.pack(side="left", fill="both", expand=True)
 
-        self.camera_frame = tk.Frame(self.left_frame, width=450, height=100, bg="black")
-        self.camera_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
-
-        self.camera_label = ttk.Label(self.left_frame, text="Camera Label")
-        self.camera_label.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+        self.camera_label = ttk.Label(self.left_frame, text="Camera Preview")
+        self.camera_label.pack(padx=5, pady=5, anchor="n")
+        self.camera_frame = tk.Frame(self.left_frame, width=460, height=270, bg="black")
+        self.camera_frame.pack(padx=5, fill="both", expand=True)
+        self.camera_placeholder = ttk.Label(self.camera_frame, text="Preview currently not implemented")
+        self.camera_placeholder.pack()
 
         # Right frame #######################################################
-        self.right_frame = tk.Frame(root, width=480, height=500, bg="green")
-        self.right_frame.grid(row=0, column=1, sticky="nsew")
+        self.right_frame = tk.Frame(self.content, width=480, height=500, bg="green")
+        self.right_frame.pack(side="left", fill="both", expand=True)
+
+        self.textfield = tk.Text(self.right_frame, wrap="word", height=100, width=100)
 
         self.output_frame = ttk.Frame(self.right_frame, width=450, height=300, style="TFrame")
-        self.output_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
-
+        self.output_frame.pack(padx=5, pady=5, fill="both", expand=True)
         self.output_label = ttk.Label(self.right_frame, text="Output Label")
-        self.output_label.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
-
+        self.output_label.pack(padx=5, pady=5, anchor="n")
         self.specto_button = ttk.Button(self.right_frame, text="Spectogram", width=10, command=self.spectogram)
-        self.specto_button.grid(row=1, column=0, padx=5, pady=5, sticky="sw")
+        self.specto_button.pack(padx=5, pady=5, anchor="sw")
+        self.discard_button = ttk.Button(self.right_frame, text="Discard", width=10, command=self.discard)
+        self.discard_button.pack(padx=5, pady=5, anchor="sw")
 
-        self.discard_button = ttk.Button(self.right_frame, text="Discard", width=10)
-        self.discard_button.grid(row=2, column=0, padx=5, pady=5, sticky="sw")
+        # Bottom frame ######################################################
+        self.bottom_frame = tk.Frame(self.root)
+        self.bottom_frame.pack(side="bottom", fill="x")
 
-        # Quit ############################################################
-        self.quit_button = ttk.Button(self.root, text="Quit", command=root.destroy)
-        self.quit_button.grid(row=2, column=0, padx=5, pady=5, sticky="sw")
+        self.quit_frame = tk.Frame(self.bottom_frame)
+        self.quit_frame.pack(padx=5, pady=5, side="left")
+        self.quit_button = ttk.Button(self.quit_frame, text="Quit", command=root.destroy)
+        self.quit_button.pack(padx=5, pady=5, anchor="w")
 
-        # Record ##########################################################
-        self.record_button = ttk.Button(self.root, text="Record", command=self.start_recording)
-        self.record_button.grid(row=2, column=1, padx=5, pady=5, sticky="se")
+        self.record_frame = tk.Frame(self.bottom_frame)
+        self.record_frame.pack(padx=5, pady=5, side="right")
+        self.record_button = ttk.Button(self.record_frame, text="Record", command=self.start_recording)
+        self.record_button.pack(padx=5, pady=5, anchor="e")
 
-        # self.root.grid_rowconfigure(0, weight=1)
-        # self.left_frame.grid_rowconfigure(0, weight=1)
-        # self.right_frame.grid_rowconfigure(0, weight=1)
+        self.bottom_frame.pack_propagate(False)
+        self.bottom_frame.pack_propagate(True)
+
+        self.content.pack(fill="both", expand=True)
 
         self.controller = GUIAppController(self)
 
@@ -69,3 +75,6 @@ class GUIApp:
 
     def spectogram(self):
         self.controller.show_spectogram()
+
+    def discard(self):
+        self.controller.discard_record()
--- sources/user_interface/GUIAppController.py
+++ sources/user_interface/GUIAppController.py
@@ -9,7 +9,7 @@ from sources.TestVisualization import plot_spectogram, plot_spectogram2
 class GUIAppController:
     def __init__(self, gui_app):
         self.gui_app = gui_app
-        self.latest_audio_path = "data/records/Major_0.wav"
+        self.latest_audio_path = ""
         self.latest_image_path = ""
 
     def start_chord_detection(self):
@@ -26,18 +26,35 @@ class GUIAppController:
             audio_stream = AudioStream(index)
             self.latest_audio_path = audio_stream.record_audio()
             print(f"Recorded audio stored here: {self.latest_audio_path}")
+        else:
+            self.latest_audio_path = "data/records/Major_0.wav"
 
         # Find chord (record = CNN input, chord = CNN output) and capture image
         cd = ChordDetector(self.latest_audio_path)
         chord = cd.classify_chord()
         if chord in CLASSES:
             print(f"Recorded chord is: {chord}. \nImage will be captured.")
-            self.latest_image_path = webcam.capture_image(self.latest_audio_path)
-            print(f"Image stored here: {self.latest_image_path}")
+            try:
+                self.latest_image_path = webcam.capture_image(self.latest_audio_path)
+                print(f"Image stored here: {self.latest_image_path}")
+            except OSError:
+                print("An error occurred: Camera not reachable.")
 
         self.gui_app.record_button["state"] = "normal"
 
     def show_spectogram(self):
-        print("Showing mel-spectogram of audio")
-        # plot_spectogram(self.latest_audio_path)
-        plot_spectogram2(self.latest_audio_path)
+        if self.latest_audio_path != "":
+            print("Showing mel-spectogram of audio")
+            # plot_spectogram(self.latest_audio_path)
+            plot_spectogram2(self.latest_audio_path)
+        else:
+            print("Record audio first.")
+
+    def discard_record(self):
+        if self.latest_audio_path == "" or self.latest_image_path == "":
+            print("Recording audio needed to discard record.")
+        else:
+            print("Recorded audio and captured image will be deleted.")
+            # todo: delete feature
+            self.latest_audio_path = ""
+            self.latest_audio_path = ""
