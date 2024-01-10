eb089358cf2a10a1e44cead9a55035d735ea1718
manuelkeck <72602863+manuelkeck@users.noreply.github.com>
2024-01-10 18:12:13 +0100

    Refactored images paths and added train_model folder to src/

---
+++ src/train_model/TrainModelHelpers.py
@@ -0,0 +1,19 @@
+"""
+Author: Manuel Keck
+This file contains several functions which are needed/helpful preparations
+to get an own trained model.
+"""
+
+
+def crop_images():
+    pass
+
+
+def get_model():
+    # Return google net model
+    pass
+
+
+def split_dataset():
+    # 80 % training data, 10 % testing data, 10 % validation data
+    pass
